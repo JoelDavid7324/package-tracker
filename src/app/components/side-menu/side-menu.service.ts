57208e2793daf96d0d17643e36f9979f915c1e11
JoelDavid7324 <159042638+JoelDavid7324@users.noreply.github.com>
2025-09-24 11:03:05 -0400

    animacion del sidebar lista

--- src/app/components/side-menu/side-menu.service.ts
+++ src/app/components/side-menu/side-menu.service.ts
@@ -8,7 +8,6 @@ export class SideMenuService {
         return this.side_menu_isActive()
     }
     setSideMenuStatus() {
-        this.side_menu_isActive() === false ? this.side_menu_isActive.set(true) : this.side_menu_isActive.set(false)
-        console.log(this.side_menu_isActive())
+        this.side_menu_isActive.update((a: boolean) => !a)
     }
 }
